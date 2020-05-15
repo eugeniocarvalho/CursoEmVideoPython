d70ae878429e2b63a0590731326d0226327507fa
Eugênio Freire <eugenio9008@gmail.com>
2020-05-15 20:38:41 -0300

    initial commit

---
+++ Python Exercicios/Mundo 1: Fundamentos/4. Condições em Python (if..else)/ex035.py
@@ -0,0 +1,18 @@
+'''
+Faça um progeama que leia o comprimento de tres retas e
+diga ao usuario se elas podem ou não formar um triangulo
+
+Para construir um triângulo é necessário que a medida de
+qualquer um dos lados seja menor que a soma das medidas
+dos outros dois e maior que o valor absoluto da diferença
+entre essas medidas
+'''
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+if abs(a - b) < c < a + b and abs(a - c) < b < a + c and abs(b - c) < a < b + c:
+    print('Forma triangulo')
+else:
+    print('Não forma triangulo')

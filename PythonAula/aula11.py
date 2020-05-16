0ea176229f3f422371b0ec4820260f00c679f670
Eugênio Freire <eugenio9008@gmail.com>
2020-05-15 21:27:06 -0300

    initial commit

---
+++ PythonAula/aula11.py
@@ -0,0 +1,10 @@
+print('\033[34mTexto\033[m')
+print('\033[30;41mTexto\033[m')
+print('\033[1;34;42mTexto\033[m')
+print('\033[31;40mTexto\033[m')
+print('\033[4;37mTexto')
+print('\033[7;37;44mTexto\033[m\n')
+
+nome = 'Eugênio'
+
+print('Olá {}{}{}, prazer em te conhecer!!!'.format('\033[32m', nome, '\033[m'))

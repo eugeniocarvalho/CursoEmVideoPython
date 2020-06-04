a6c12019c5947d6540d08716c59d8882a0d1b746
Eugênio Freire <eugenio9008@gmail.com>
2020-06-04 16:45:04 -0300

    Implemtação exemplo 36

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex036.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex036.py
@@ -1,6 +1,14 @@
 '''
 Escreva um prograa para aprovar o empréstimo bancário
-para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+para a compra de uma casa. O programa vai perguntar o
+valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 
-Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou entao o empréstimo será negado
+Calcule o valor da prestação mensal, sabendo que ela não
+pode exceder 30% do salário ou entao o empréstimo será negado
 '''
+
+valCasa = float(input('Valor da casa: '))
+salario = float(input('Salário do comprador: '))
+ano = int(input('Anos para pagar: '))
+
+

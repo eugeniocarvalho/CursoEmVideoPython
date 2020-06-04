d7c1f095795ca9216dc5757358a5e0ea021138df
Eugênio Freire <eugenio9008@gmail.com>
2020-06-04 16:45:22 -0300

    Implemtação exemplo 40

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex040.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex040.py
@@ -1,7 +1,19 @@
-'''
+"""
 Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
 
 - média abaixo de 5 : reprovado
 - média entre 5 e 6.9 : recuperação
 - média 7 ou superior: aprovado
-'''
+"""
+
+nota1 = float(input(' '))
+nota2 = float(input(' '))
+
+media = (nota1 + nota2) / 2
+
+if media < 5:
+    print('reprovado! :(')
+elif media < 7:
+    print('Recuperação! :|')
+else:
+    print('Aprovado! :D')

a4c83d825744b1e624b5aa9326d266c366b6708c
Eugênio Freire <eugenio9008@gmail.com>
2020-06-04 16:45:11 -0300

    Implemtação exemplo 37

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex037.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex037.py
@@ -4,3 +4,19 @@ Escreva um programa que leia um número inteiro qualquer e peça para o usuário
 -2 para octal
 -3 para hexadecimal
 '''
+
+num = int(input('Numero para conversao: '))
+
+print("""
+1. Converter para binário
+2. Converter para octal
+3. Converter para Hexadecimal
+""")
+conversao = int(input())
+
+if conversao == 1:
+    print('{} convertido para binário: {}'.format(num, bin(num)))
+elif conversao == 2:
+    print('{} convertido para octal: {}'.format(num, oct(num)))
+elif conversao == 3:
+    print('{} convertido para hexadecimal: {}'.format(num, hex(num)))

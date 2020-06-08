674f4c572d4eb58bd496f193d05f8b460347f272
Eugênio Freire <eugenio9008@gmail.com>
2020-06-08 20:52:04 -0300

    Implemtação exemplo 43

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex043.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex043.py
@@ -1,9 +1,25 @@
 '''
 desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu imc e mostre seu status, de acordo com a tabela abaixo:
 
-- avaixo de 19.5: abaixo di peso
+- avaixo de 18.5: abaixo do peso
 - entre 18.5 e 25: peso ideal
 - 25 ate 30: sobrepeso
 - 30 ate 40: obesidade
-- acima de 30: obesidade móbida
+- acima de 40: obesidade móbida
 '''
+
+peso = float(input())
+altura = float(input())
+
+imc = peso / pow(altura, 2)
+
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc < 25:
+    print('Peso ideal')
+elif imc < 30:
+    print('Sobrepeso')
+elif imc < 40:
+    print('Obesidade')
+else:
+    print('Obesidade Morbida')

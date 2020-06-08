1cfccd7387e6e84a7c991b71639f8225efdd52c2
Eugênio Freire <eugenio9008@gmail.com>
2020-06-08 20:51:58 -0300

    Implemtação exemplo 42

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex042.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex042.py
@@ -5,3 +5,19 @@ Refaça o desafio 35 dos triangulos, acrescentando o recurso de mostrar que tipo
 - isosceles: dois lados iguais
 - escaleno: todos os lados diferentes
 '''
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+if abs(a - b) < c < a + b and abs(a - c) < b < a + c and abs(b - c) < a < b + c:
+    print('\033[34mForma triangulo')
+
+    if a == b and b == c:
+        print('Triângulo Equilatero')
+    elif a == b or a == c or b == c:
+        print('Triangulo Isoceles')
+    elif a != b and a != c and b != c:
+        print('Triangulo Escaleno')
+else:
+    print('\033[31mNão forma triangulo')

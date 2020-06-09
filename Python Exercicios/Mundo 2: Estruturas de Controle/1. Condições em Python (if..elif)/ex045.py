074ae802707f3e7209507accd2f0260f3dd4a0fd
Eugênio Freire <eugenio9008@gmail.com>
2020-06-09 14:46:02 -0300

    Implemtação exemplo 45

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex045.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex045.py
@@ -1,4 +1,30 @@
 '''
 Crie um programa que faça o computador jogar o jokenpo com você
 '''
+import random
+
+print("""
+1. Pedra
+2. Papel
+3. Tesoura
+""")
+player = int(input())
+
+pc = random.randrange(3)
+
+if player == pc:
+    print('Empate')
+elif player == 1 and pc == 2:
+    print('Escolhi papel. Perdeu!')
+elif player == 1 and pc == 3:
+    print('Escolhi Tesoura. Você Ganhou!')
+elif player == 2 and pc == 1:
+    print('Escolhi Pedra. Voccê Ganhou!')
+elif player == 2 and pc == 3:
+    print('Escolhi tesoura. Perdeu!')
+elif player == 3 and pc == 1:
+    print('Escolhi pedra. Perdeu!')
+elif player == 3 and pc == 2:
+    print('Escolhi papel. Você Ganhou!')
+
 

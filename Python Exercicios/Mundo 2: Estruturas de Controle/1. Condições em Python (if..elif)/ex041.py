8b57f02a8581d3b43c76db7b566735446566596f
Eugênio Freire <eugenio9008@gmail.com>
2020-06-09 15:40:29 -0300

    Implemtação exemplo 41

--- Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex041.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/1. Condições em Python (if..elif)/ex041.py
@@ -1,11 +1,29 @@
 
 '''
-A confederação nacional de notação precisa de um programa que leia o ano de nascimento de um atleta e mostra sua categoria, de acordo com a idade:
+A confederação nacional de notação precisa de um programa que leia o ano de nascimento
+de um atleta e mostra sua categoria, de acordo com a idade:
 
 - ate 9 anos: mirim
 - ate 14 anos: infantil
 - até 19 anos: junior
-- até 20 anos: senior
+- até 25 anos: senior
 - acima: master
 
 '''
+
+from datetime import date
+
+ano = int(input())
+data = date.today().year
+ano = data - ano
+if ano <= 0:
+    print('Mirim')
+elif ano <= 16:
+    print('Infantil')
+elif ano <= 19:
+    print('Junior')
+elif ano <= 25:
+    print('Senior')
+else:
+    print('Master')
+

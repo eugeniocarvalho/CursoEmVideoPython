2292cc47611a895042cc1cfacd411f62bc9eb777
Eugênio Freire <eugenio9008@gmail.com>
2020-06-11 15:34:12 -0300

    Implementação exemplo 49

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex049.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex049.py
@@ -1,3 +1,12 @@
 '''
 Refaça o desafio 9, mostrando a tabuada de um número que o usuario escolher, so que agora utilizando for
+
+DESAFIO 9:
+    Faça um programa que leia um número inteiro qualquer
+    e mostre na tela sua tabuada
 '''
+
+n = int(input(''))
+
+for i in range(10):
+    print('{} x {} = {}'.format(n, i+1, n*(i+1)))

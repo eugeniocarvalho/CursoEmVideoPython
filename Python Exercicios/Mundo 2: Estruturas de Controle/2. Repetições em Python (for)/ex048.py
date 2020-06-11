2c829878a5da96ade787b7ba7140a4eee7f6ded1
Eugênio Freire <eugenio9008@gmail.com>
2020-06-11 15:34:08 -0300

    Implementação exemplo 48

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex048.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex048.py
@@ -1,3 +1,8 @@
 '''
-Faça um programa que calcule a soma entre todos os numeros impares que sao multiplos de tres e que se encontram no intervalo de 1 ate 500
+Faça um programa que calcule a soma entre todos os numeros impares que sao
+ multiplos de tres e que se encontram no intervalo de 1 ate 500
 '''
+
+for i in range(1, 501):
+    if i % 3 == 0 and i % 2 != 0:
+        print(i)

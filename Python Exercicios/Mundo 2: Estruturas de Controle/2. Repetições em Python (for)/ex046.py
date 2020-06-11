b3e2e1d9a380f081714eaf3defa527e031d3629d
Eugênio Freire <eugenio9008@gmail.com>
2020-06-11 15:33:56 -0300

    Implementação exemplo 46

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex046.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex046.py
@@ -1,3 +1,11 @@
 '''
-Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogo de artificio, indo de 10 ate 0, com uma pause de 1 segundo entre eles
+Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogo de artificio,
+indo de 10 ate 0, com uma pause de 1 segundo entre eles
 '''
+import time
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print('Feliz ano novo!!')

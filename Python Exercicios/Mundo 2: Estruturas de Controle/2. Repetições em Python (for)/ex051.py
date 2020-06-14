36767601e6769b03c0a59aebf5bd6e3a621aab86
Eugênio Freire <eugenio9008@gmail.com>
2020-06-13 22:13:28 -0300

    Implementação exemplo 51

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex051.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex051.py
@@ -1,3 +1,12 @@
 '''
-Desenvolva um programa que leia o primeiro termo e a razao de uma PA, No final, mostre os 10 primeiros termos dessa progressao
+Desenvolva um programa que leia o primeiro termo e a razao de uma PA,
+No final, mostre os 10 primeiros termos dessa progressao
+
+an = a1 + (n – 1)r
 '''
+
+a1 = int(input())
+r = int(input())
+
+for i in range(1, 10):
+    print(a1 + (i - 1) * r)

f64200de8e9a3878b3d54750bea7c98207db2bbb
Eugênio Freire <eugenio9008@gmail.com>
2020-06-13 22:13:23 -0300

    Implementação exemplo 50

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex050.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex050.py
@@ -1,3 +1,14 @@
 '''
-Desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for impar, descondere-o
+Desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas
+daqueles que forem pares. Se o valor digitado for impar, descondere-o
 '''
+
+sum = 0
+
+for i in range(6):
+    num = int(input())
+
+    if num % 2 == 0:
+        sum += num
+
+print('Soma dos numeros pares {}'.format(sum))

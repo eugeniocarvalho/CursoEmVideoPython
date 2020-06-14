4801cd0ebbeb3a15a7e1a32aadecd1c12701a32e
Eugênio Freire <eugenio9008@gmail.com>
2020-06-13 22:13:39 -0300

    Implementação exemplo 53

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex053.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex053.py
@@ -1,3 +1,19 @@
 '''
 Crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando os espaços
 '''
+
+frase = input().strip()
+j = len(frase) - 1
+
+i = 0
+
+for i in range(len(frase)):
+    if frase[i] == frase[j]:
+        j -= 1
+    else:
+        break
+
+if j < 0:
+    print('É palindromo')
+else:
+    print('Não é palindromo')

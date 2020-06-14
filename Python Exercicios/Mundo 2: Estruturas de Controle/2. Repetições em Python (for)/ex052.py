bd0d4be86f5e50ca1b6619715722f9d7af072bd4
Eugênio Freire <eugenio9008@gmail.com>
2020-06-13 22:13:34 -0300

    Implementação exemplo 52

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex052.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex052.py
@@ -1,3 +1,15 @@
 '''
 Faça um programa qu leia um numero inteiro e diga se ele e ou nao numero primo
 '''
+
+num = int(input())
+sum = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        sum += 1
+
+if sum <= 2:
+    print('É primo')
+else:
+    print('Não é primo')

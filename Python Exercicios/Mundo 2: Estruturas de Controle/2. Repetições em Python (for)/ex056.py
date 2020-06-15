5cb6794fa893d67711cc82a983fac9e282d0dd97
Eugênio Freire <eugenio9008@gmail.com>
2020-06-15 10:40:15 -0300

    Implementação exemplo 56

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex056.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex056.py
@@ -4,3 +4,28 @@ Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do p
 - QUal é o nome do homem mais velho
 - Quantas mulheres tem menos de 20 anos
 '''
+
+mediaIdade = 0
+maiorIdade = 0
+nomeMaisVelho = ''
+cont = 0
+
+for i in range(4):
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('Sexo: ')
+    print()
+
+    mediaIdade += idade
+
+    if sexo == 'f' or sexo == 'F':
+        if idade < 20:
+            cont += 1
+    elif sexo == 'm' or sexo == 'M':
+        if idade > maiorIdade:
+            maiorIdade = idade
+            nomeMaisVelho = nome
+
+print('Média de idade: {}'.format(mediaIdade / 4))
+print('Nome do homem mais velho: {}'.format(nomeMaisVelho))
+print('Quantidade de mulher com menos de 20 anos: {}'.format(cont))

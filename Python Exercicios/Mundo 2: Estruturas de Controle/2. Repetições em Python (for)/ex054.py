7c017e3a494234105f1d2b46969362627ca2e815
Eugênio Freire <eugenio9008@gmail.com>
2020-06-15 10:40:05 -0300

    Implementação exemplo 54

--- Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex054.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/2. Repetições em Python (for)/ex054.py
@@ -1,3 +1,21 @@
 '''
-Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda nao atingiram a maioridade e quantas ja sao maiores
+Crie um programa que leia o ano de nascimento de sete pessoas.
+No final, mostre quantas pessoas ainda nao atingiram a maioridade e quantas ja sao maiores
 '''
+
+import datetime
+
+atual = datetime.date.today().year
+menor = 0
+maior = 0
+
+for i in range(7):
+    ano = int(input())
+
+    if atual - ano < 18:
+        menor +=1
+    else:
+        maior +=1
+
+print('Peesoas de menor: {}'.format(menor))
+print('Pessoas de maior: {}'.format(maior))

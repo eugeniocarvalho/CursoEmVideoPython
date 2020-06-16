72d9d4e484c510859d3baa4445aca2bc0393ef49
Eugênio Freire <eugenio9008@gmail.com>
2020-06-16 20:31:51 -0300

    Initial commit

---
+++ PythonAula/aula14.py
@@ -0,0 +1,29 @@
+c = 1
+
+while c < 10:
+    print(c)
+    c += 1
+
+print('Fim')
+
+s = 's'
+
+while s == 's':
+    n = int(input('Digite um numero: '))
+    s = input('Quer continuar? S/N').upper()
+
+print('Fim')
+
+p = i = 0
+n = 1
+
+while n:
+    n = int(input('Digite um numero: '))
+
+    if n != 0:
+        if n % 2 == 0:
+            p += 1
+        else:
+            i += 1
+
+print('{} numero pares e {} numeros impares'.format(p, i))

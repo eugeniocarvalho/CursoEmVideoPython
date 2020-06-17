499a6c21bcfd96966b85e2e9388bc1b33e3e2528
Eugênio Freire <eugenio9008@gmail.com>
2020-06-17 17:20:39 -0300

    Implementação exemplo 60

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex060.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex060.py
@@ -1,3 +1,14 @@
 '''
 Faça um programa que leia um numero qualquer e mostre o seu fatorial
 '''
+
+n = int(input())
+fat = 1
+
+print('Fatorial de {}'.format(n), end='')
+
+while n > 1:
+    fat *= n
+    n -= 1
+
+print(' = {}'.format(fat))

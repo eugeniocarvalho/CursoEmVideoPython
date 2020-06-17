595036e1b4909979ae5dad2160efaa57d05b3a64
Eugênio Freire <eugenio9008@gmail.com>
2020-06-17 17:20:24 -0300

    Implementação exemplo 57

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex057.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex057.py
@@ -2,3 +2,10 @@
 Faça um programa que leia o sexo de uma pessoa, mas so aceita os valore M ou F.
  caso esteja errado peça a digitação novamente ate tter um valor correto
 '''
+
+sexo = input()
+
+while sexo != 'F' and sexo != 'M':
+    print('Digite sexo novamente (M/F)')
+    sexo = input()
+

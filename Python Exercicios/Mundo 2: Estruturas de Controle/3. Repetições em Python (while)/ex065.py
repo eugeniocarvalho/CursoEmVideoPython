a15aab29a4e28fbe1c942ef52b2bc996041352bf
Eugênio Freire <eugenio9008@gmail.com>
2020-06-18 18:34:48 -0300

    Implementação exemplo 65

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex065.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex065.py
@@ -1,5 +1,25 @@
 '''
-Cire um programa que leia varios numeros inteiros pelo teclado.
+Crie um programa que leia varios numeros inteiros pelo teclado.
 No final da execição mostre media entre todos os valores e qual foi o maior e menor valor lido.
 O programa deve perguntar ao usuario se ele quer ou nao continuar a digitar valores
 '''
+
+cont = sum = 0
+c = 1
+maior = menor = 0
+while c:
+    n = int(input())
+    if cont == 0:
+        menor = n
+
+    if n > maior:
+        maior = n
+
+    if n < menor:
+        menor = menor
+
+    cont += 1
+    sum += n
+    c = int(input())
+
+print('Media: {}, maior: {}, menor: {}'.format(sum/cont, maior, menor))

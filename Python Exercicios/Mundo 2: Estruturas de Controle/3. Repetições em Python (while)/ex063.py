11153953c0df80fde5620917dad3d3d80e5dde40
Eugênio Freire <eugenio9008@gmail.com>
2020-06-18 18:24:18 -0300

    Initial commit

---
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex063.py
@@ -0,0 +1,6 @@
+'''
+Escreva um programa que leia um numero n inteiro qualquer e
+mostre na tela os n primeiros elementos de uma sequencia de Fibonacci
+'''
+
+
---
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex065.py
@@ -0,0 +1,5 @@
+'''
+Cire um programa que leia varios numeros inteiros pelo teclado.
+No final da execição mostre media entre todos os valores e qual foi o maior e menor valor lido.
+O programa deve perguntar ao usuario se ele quer ou nao continuar a digitar valores
+'''

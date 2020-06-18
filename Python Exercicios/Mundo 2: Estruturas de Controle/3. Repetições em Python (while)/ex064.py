8af379af456085202f820d1cf00572a7048db3f4
Eugênio Freire <eugenio9008@gmail.com>
2020-06-18 18:27:52 -0300

    Implementação exemplo 64

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex064.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex064.py
@@ -3,3 +3,13 @@ Crie um programa que leia varios numero inteiros pelo teclado.
 O programa só vai parar quando o usuario digitar o valor 999. que é a condição de parada.
 No final, mostre quantos numeros foram digitados e qual foi a soma entre eles
 '''
+
+n = int(input())
+sum = cont = 0
+
+while n != 999:
+    sum += n
+    cont += 1
+    n = int(input())
+
+print('{} numeros digitados, e a soma deles é: {}'.format(cont, sum))

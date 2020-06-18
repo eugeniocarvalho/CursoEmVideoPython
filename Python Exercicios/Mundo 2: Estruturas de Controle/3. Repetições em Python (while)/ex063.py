de23c24ff2860aa20ee4eaf7a229db90dd9b15a7
Eugênio Freire <eugenio9008@gmail.com>
2020-06-18 18:24:40 -0300

    Implementação exemplo 63

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex063.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex063.py
@@ -3,4 +3,14 @@ Escreva um programa que leia um numero n inteiro qualquer e
 mostre na tela os n primeiros elementos de uma sequencia de Fibonacci
 '''
 
+n = int(input())
 
+a = f = i = 0
+b = 1
+
+while i < n:
+    print(f, end=' ')
+    f = a + b
+    b = a
+    a = f
+    i += 1

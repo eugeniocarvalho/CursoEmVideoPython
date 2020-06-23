d274c2ad9c3da653e1861551f6df92d01564a5bd
Eugênio Freire <eugenio9008@gmail.com>
2020-06-23 11:19:47 -0300

    Implementação exemplo 67

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex067.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex067.py
@@ -3,3 +3,13 @@ Faça um programa que mostre a tabuada de varios números,
 um de cada vez, para cada valor digitado pelo usuario.
 O programa sera interrmpido quando o numero solicitado for negativo
 '''
+
+while True:
+    n = int(input())
+
+    if n != 0:
+        for i in range(11):
+            print(f'{n} x {i} = {i * n}')
+    else:
+        break
+    print()

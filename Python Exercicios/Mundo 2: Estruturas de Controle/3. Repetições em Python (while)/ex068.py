c18ef9239aad7b4fd71e9a1fe4d528d6bc889e45
Eugênio Freire <eugenio9008@gmail.com>
2020-06-23 11:19:54 -0300

    Implementação exemplo 68

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex068.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex068.py
@@ -3,3 +3,30 @@ Faça um programa que jogue par ou impar com o computdaor.
 O jogo só sera interrrompido quando o jogador perder,
 mostrando o total de vitórias consecutivas que ele conquistou no final do jogo
 '''
+from random import randrange
+
+num = 0
+
+while True:
+    player = int(input())
+    pc = randrange(10)
+
+    opcao = input('Par ou Impar? P/I: ').upper()
+    print(f'Você jogou {player} e o computador jogou {pc}')
+
+    if opcao == 'P':
+        if (player + pc) % 2 == 0:
+            print('Voccê ganhou!!')
+            num += 1
+        else:
+            print('Você perdeu :/')
+            break
+    elif opcao == 'I':
+        if (player + pc) % 2 != 0:
+            print('Voccê ganhou!!')
+            num += 1
+        else:
+            print('Você perdeu :/')
+            break
+
+print(f'Você ganhou {num} vezes conssecutivas')

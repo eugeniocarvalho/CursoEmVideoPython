8261c7c37ce92e48e1f47a96f52d01fa65104b5d
Eugênio Freire <eugenio9008@gmail.com>
2020-06-23 11:19:42 -0300

    Implementação exemplo 66

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex066.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex066.py
@@ -4,3 +4,16 @@ O programa só vai parar quando o usuario digitar o valor 999,
 que é a condição de parada. No final, mostre quantos números foram
 digitados e qual foi a soma entre eles
 '''
+
+sum = total = 0
+
+while True:
+    n = int(input())
+
+    if n == 999:
+        break
+
+    sum += n
+    total += 1
+
+print(f'Total de {total} números e o somatório é: {sum}')

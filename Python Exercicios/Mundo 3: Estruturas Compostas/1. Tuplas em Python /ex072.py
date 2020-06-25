0a12374deb1a0590a36ee177dead1bb2d0108279
Eugênio Freire <eugenio9008@gmail.com>
2020-06-25 20:32:56 -0300

    Implementação exemplo 72

--- Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex072.py
+++ Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex072.py
@@ -4,4 +4,13 @@ Crie um prgrama que tenha uma trupla totalmente preenchida com uma contagem por 
 Seu programa devera ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenção
 '''
 
+numeros = (
+'zero', 'um', 'dois', 'trẽs', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+'cartoze', 'quinze', 'dezessseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
+n = int(input('Digite um numero [0:20]: '))
+
+while n not in range(0, 21):
+    n = int(input('Digite um numero [0:20]: '))
+
+print(f'Você digitou o numero {numeros[n]}')

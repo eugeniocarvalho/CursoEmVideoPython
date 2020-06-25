f0284e095dd1063754822ba60faed98074d17b07
Eugênio Freire <eugenio9008@gmail.com>
2020-06-24 21:36:17 -0300

    Implementação exemplo 70

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex070.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex070.py
@@ -6,3 +6,32 @@ O programa devera perguntar se o usuario vai continuar. No final, mostre:
     B. quantos produtos custam mais de 1000
     C. qual é o nome do produto mais barato
 '''
+
+total = qtd = menor = 0
+barato = ''
+
+while True:
+    nome = input('Nome do produto: ')
+    preco = float(input('Preço: '))
+
+    if preco > 1000:
+        qtd += 1
+
+    if total == 0:
+        menor = preco
+        barato = nome
+
+    total += preco
+
+    if preco < menor:
+        menor = preco
+        barato = nome
+
+    opção = input('Quer continuar? S/N ').upper()
+
+    if opção != 'S':
+        break
+
+print(f'Total gasto de R$ {total:.2f}')
+print(f'{qtd} produtos custam mais de R$ 1000,00')
+print(f'O produto mais barato é {barato}, que custa {menor:.2f}')

996dbbfd8ebd828843992aafaf5c6157f3376740
Eugênio Freire <eugenio9008@gmail.com>
2020-06-25 20:33:05 -0300

    Implementação exemplo 74

--- Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex074.py
+++ Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex074.py
@@ -4,3 +4,8 @@ Crie um programa que vai gerar cinco numeros aleatórios e colocar em uma tupla
 Depois disso, mostre a listagem e numeros gerados e tambem indique o menor e o maior valor que estao na tupla
 '''
 
+from random import randint
+
+num = (randint(5))
+
+print(num)

0a30664cbf6b18bda2d7ac53afea571d789f0655
Eugênio Freire <eugenio9008@gmail.com>
2020-06-24 22:11:02 -0300

    Implementação exemplo 71

--- Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex071.py
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex071.py
@@ -5,3 +5,25 @@ e o programa vai informr quantas cedulas de cada valor serao entregues
 
 Obs: Considere que o caixa possui cedulas de 50, 20, 10 e 1
 '''
+
+sacar = int(input('Quantos você quer sacar? '))
+
+a = b = c = d = 0
+
+while sacar:
+    a = sacar // 50
+    sacar %= 50
+
+    b = sacar // 20
+    sacar %= 20
+
+    c = sacar // 10
+    sacar %= 10
+
+    d = sacar // 1
+    sacar %= 1
+
+print(f'{a} notas de R$ 50,00')
+print(f'{b} notas de R$ 20,00')
+print(f'{c} notas de R$ 10,00')
+print(f'{d} notas de R$ 1,00')

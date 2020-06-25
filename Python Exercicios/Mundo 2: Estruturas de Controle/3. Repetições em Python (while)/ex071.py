ab35469ff5d8ad08a1fffe1d5427d7b590179a00
Eugênio Freire <eugenio9008@gmail.com>
2020-06-24 22:10:52 -0300

    Initial Commit

---
+++ Python Exercicios/Mundo 2: Estruturas de Controle/3. Repetições em Python (while)/ex071.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que simule o funcionamento de um caixa eletronico.
+No inicio, pergunte ao usuario qual sera o valor a ser sacado (numero int)
+e o programa vai informr quantas cedulas de cada valor serao entregues
+
+Obs: Considere que o caixa possui cedulas de 50, 20, 10 e 1
+'''

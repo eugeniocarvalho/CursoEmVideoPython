b2b2c5f4f2f2ba3b00c9a6b1cb08a94954ced0e5
Eugênio Freire <eugenio9008@gmail.com>
2020-06-26 09:57:35 -0300

    Initial commit

---
+++ Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex075.py
@@ -0,0 +1,6 @@
+'''
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. no final, mostre:
+    a. quantas vezes apareceu o valor 9
+    b. em que posição foi digitado o primeiro valor 3
+    c. quais foram os numeros pares
+'''
---
+++ Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex077.py
@@ -0,0 +1,5 @@
+'''
+Crie um programa que tenha uma tupla com varias palavras (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+
+'''

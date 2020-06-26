816f5f4812bb1d5bc9ff02fc4e6680235cfcac85
Eugênio Freire <eugenio9008@gmail.com>
2020-06-26 10:33:59 -0300

    Implementação exemplo 75

--- Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex075.py
+++ Python Exercicios/Mundo 3: Estruturas Compostas/1. Tuplas em Python /ex075.py
@@ -4,3 +4,18 @@ Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma t
     b. em que posição foi digitado o primeiro valor 3
     c. quais foram os numeros pares
 '''
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+
+tupla = (n1, n2, n3, n4)
+
+print(f'O valor 9 apareceu {tupla.count(9)} vezes')
+print(f'O valor 3 esta na {tupla.index(3) + 1}º posição')
+print('Números pares: ', end='')
+
+for i in tupla:
+    if i % 2 == 0:
+        print(i, end=' ')
